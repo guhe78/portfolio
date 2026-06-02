e1d3a9183c67788fbcb9b0a9ca4f8164a4d9646b
Guenter Heldt <gh1978@hotmail.de>
2026-06-02 17:47:53 +0200

    refactor: sections

---
+++ script.js
@@ -0,0 +1,17 @@
+const programmingLanguages = [
+  { name: "HTML5", icon: "html" },
+  { name: "CSS3", icon: "css" },
+  { name: "JavaScript", icon: "javascript" },
+  { name: "Git", icon: "git" },
+];
+
+const container = document.querySelector(".skill-badges-container");
+
+programmingLanguages.forEach((lang) => {
+  container.innerHTML += `
+    <div class="skill-item">
+      ${icons[lang.icon](30)}
+      <p>${lang.name}</p>
+    </div>
+  `;
+});
