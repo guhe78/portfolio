629ad08d2b3ab113c950fe178b4a15c16630a955
Guenter Heldt <gh1978@hotmail.de>
2026-06-22 11:28:21 +0200

    refactor styling sections

--- scripts/skill-section.js
+++ scripts/skill-section.js
@@ -3,10 +3,12 @@ const programmingLanguages = [
   { name: "CSS3", icon: "css" },
   { name: "JavaScript", icon: "javascript" },
   { name: "Git", icon: "git" },
+  { name: "Scrum", icon: "scrum" },
+  { name: "Growth mindset", icon: "growth_mindset" },
 ];
 
 function createSkillBadgeContainer() {
-  const container = document.querySelector(".skill-badges-container");
+  const container = document.getElementById("skill-badges-container");
 
   programmingLanguages.forEach((lang) => {
     container.innerHTML += `
