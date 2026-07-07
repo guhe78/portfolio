e348b322fd4e7adad3496aee67729f1169405e81
Guenter Heldt <gh1978@hotmail.de>
2026-07-07 10:10:54 +0200

    added jsdoc comments

--- scripts/aboutme-section.js
+++ scripts/aboutme-section.js
@@ -1,7 +1,15 @@
+/**
+ * Initializes the "About Me" section of the webpage by setting up the icons and text.
+ * This function is called when the "About Me" section is loaded to ensure that the icons and text are properly displayed.
+ */
 function initAboutMeSection() {
   initAboutMeText();
 }
 
+/**
+ * Initializes the "About Me" section by setting up the icons and text.
+ * This function is called when the "About Me" section is loaded to ensure that the icons and text are properly displayed.
+ */
 function initAboutMeText() {
   const aboutMeDescriptionLocation = document.getElementById("location-icon");
   const aboutMeDescriptionCognition = document.getElementById("cognition-icon");
