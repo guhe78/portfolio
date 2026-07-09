af1e1cd50f70f1e6ab19126a35fcbb3d82705281
Guenter Heldt <gh1978@hotmail.de>
2026-07-09 09:41:54 +0200

    fixed problems

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,20 +2,7 @@
  * Initializes the footer by setting up event listeners for the footer elements.
  */
 function initFooter() {
-  initFooterEventListeners();
   setScrollUpArrow();
-}
-
-/**
- * Sets up event listeners for the footer elements, such as the impressum link.
- */
-function initFooterEventListeners() {
-  const footerImpressum = document.getElementById("footer-impressum");
-
-  footerImpressum.addEventListener("click", () => {
-    const currentLanguage = localStorage.getItem("language") || "de";
-    openLegalDialog(currentLanguage);
-  });
 }
 
 /**
--- scripts/legal-privacy.js
+++ scripts/legal-privacy.js
@@ -1,4 +1,5 @@
 function initLegalPrivacy() {
   initLanguage();
   initFooter();
+  toggleMobileMenu();
 }
