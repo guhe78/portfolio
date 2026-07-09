fe94ca6ab0fd7d1eda22dc215b1243cc6eb08e7d
Guenter Heldt <gh1978@hotmail.de>
2026-07-09 09:47:03 +0200

    refactor jsdoc

--- scripts/legal-privacy.js
+++ scripts/legal-privacy.js
@@ -1,3 +1,7 @@
+/**
+ * Initializes the legal privacy page by setting up language options, footer, and mobile menu toggle functionality.
+ * This function is called when the legal privacy page is loaded to ensure that the language settings, footer, and mobile menu are properly initialized.
+ */
 function initLegalPrivacy() {
   initLanguage();
   initFooter();
